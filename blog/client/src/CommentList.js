dcb02f0d6c272149944c18192a072db7aa411fad
Eugene <e.v.e.r.e.s.tt.1551@gmail.com>
2024-10-19 12:18:36 +0200

    posts with comments (post & get)

---
+++ blog/client/src/CommentList.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState} from 'react'
+import axios from 'axios'
+import CommentCreate from './CommentCreate'
+
+export default ({postId}) => {
+    
+    const [comments, setComments] = useState([])
+
+    const fetchComments = async () => {
+        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
+        setComments(res.data)
+    }
+
+    useEffect(() => {
+        fetchComments()
+    }, [])
+
+    const renderedComments = comments.map(comment => {
+        return <li key={comment.id}>
+            {comment.content}
+        </li> 
+    })
+   
+    return <ul className=''>
+        {renderedComments}
+    </ul>
+}
